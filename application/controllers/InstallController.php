7b7d784a757df3c7ed38557d842169fcf59a9da8
Joseph Montanez <jmontanez@gorilla3d.com>
2010-07-11 10:49:24 -0700

    working on installer

--- application/controllers/InstallController.php
+++ application/controllers/InstallController.php
@@ -1,18 +1,43 @@
 <?php
 
-class InstallController extends Zend_Controller_Action
-{
+class InstallController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
+        $this->_helper->_layout->setLayout('install');
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
-    }
+        $adaptors = array(
+            'IBM DB2 and Informix Dynamic Server (IDS)' => 'pdo_ibm',
+            'MySQL' => 'pdo_mysql',
+            'MySQLi' => 'mysqli',
+            'Microsoft SQL Server' => 'pdo_mssql',
+            'Oracle' => 'pdo_oci',
+            'PostgreSQL' => 'pdo_pgsql',
+            'Sqlite' => 'pdo_sqlite'
 
+        );
+        foreach($adaptors as $key => $adaptor) {
+            $db = Zend_Db::factory($adaptor, array(
+                'dbname' => 'test',
+                'password' => 'test',
+                'username' => 'test'
+            ));
+            try {
+                $db->query('SELECT * FROM test');
+            } catch (Exception $e) {
+                if(stristr($e->getMessage(), 'not currently installed')) {
+                    unset($adaptors[$key]);
+                }
+            }
+        }
+        $this->view->adaptors = $adaptors;
+
+
+        $form = new Default_Form_InstallStep1();
+        $this->view->form = $form;
+    }
 
 }
-
